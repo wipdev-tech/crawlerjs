b1a9f2ac17dc4eb4388786b072522eb9090d3108
wipdev-tech <wipdev.tech@gmail.com>
2023-10-27 12:47:04 +0200

    Added `getURLsFromHTML` function

--- crawl.js
+++ crawl.js
@@ -1,8 +1,18 @@
 module.exports = {
-    normalizeUrl
+    normalizeUrl,
+    getUrlsFromHTML
 }
 
+const { JSDOM } = require("jsdom")
+
 function normalizeUrl(u) {
-    url = new URL(u)
+    const url = new URL(u)
     return url.host + url.pathname
 }
+
+function getUrlsFromHTML(htmlBody, baseURL) {
+    const dom = new JSDOM(htmlBody)
+    const anchors = Array.from(dom.window.document.querySelectorAll('a'))
+    return anchors.map(a => baseURL + a.href.replace("about:blank", ""))
+}
+
--- crawl.test.js
+++ crawl.test.js
@@ -1,6 +1,7 @@
 const { test, expect } = require('@jest/globals')
-const { normalizeUrl } = require('./crawl.js')
+const { normalizeUrl, getUrlsFromHTML: getURLsFromHTML } = require('./crawl.js')
 
+// normalizeUrl tests
 test("trailing slash is ignored", () => {
     expect(normalizeUrl("https://duckduckgo.com/"))
         .toBe(normalizeUrl("https://duckduckgo.com"))
@@ -19,4 +20,27 @@ test("search params are ignored", () => {
 test("hash is ignored", () => {
     expect(normalizeUrl("https://duckduckgo.com/#searchbox_input"))
         .toBe(normalizeUrl("https://duckduckgo.com/"))
+})
+
+
+// getUrlsFromHTML tests
+test("urls are extracted", () => {
+    const htmlStr = `<p>I did talk about JS noise briefly in the <a href="/posts/developer-philosophies/">Developer Philosophies</a> article, although I wrote that... ... attention to a language’s common philosophies and practices (and I contemplated that topic in yet another article called <a href="/posts/pragmatism-vs-idealism/">Pragmatism vs Idealism</a>).</p> <a href="/">Home</a>`
+
+    expect(getURLsFromHTML(htmlStr, "https://wipdev.netlify.app"))
+        .toStrictEqual([
+            "https://wipdev.netlify.app/posts/developer-philosophies/",
+            "https://wipdev.netlify.app/posts/pragmatism-vs-idealism/",
+            "https://wipdev.netlify.app/"
+        ])
+})
+
+test("`about:blank` is removed", () => {
+    const htmlStr = `<div class="onboardingEd_onboardingEdDismiss__WnMRt"><a href="#" class="onboardingEd_dismissBtn__PZMVz">Dismiss forever</a><span class="onboardingEd_divider__ICbPu">|</span><a href="#" class="onboardingEd_backToSearchBtn__81cxP">Back to search</a></div>`
+
+    expect(getURLsFromHTML(htmlStr, "https://duckduckgo.com/"))
+        .toStrictEqual([
+            "https://duckduckgo.com/#",
+            "https://duckduckgo.com/#",
+        ])
 })
