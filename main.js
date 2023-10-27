2901d008aa286f15f7f655e167656fb1c47e293e
wipdev-tech <wipdev.tech@gmail.com>
2023-10-27 13:42:19 +0200

    Initial main.js that takes a command-line argument

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hello world!")
+function main() {
+    if (process.argv.length < 3) {
+        console.log("Error: no arguments provided (need exactly 1)")
+        return
+    }
+
+    if (process.argv.length > 3) {
+        console.log("Error: too many arguments provided (need exactly 1)")
+        return
+    }
+
+    console.log(`Crawling URL ${process.argv[2]}...`)
+}
+
+main()
